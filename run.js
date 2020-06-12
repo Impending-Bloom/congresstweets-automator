2eb1b1dd504e3b1ce35ad25b22d1902495a4e0e0
Joonho Han <joonhojhan@gmail.com>
2020-06-12 15:05:41 -0400

    updates some node modules and json files in data folder

--- run.js
+++ run.js
@@ -1,15 +1,26 @@
 const { exec } = require('child_process');
 
-setInterval(() => {
-	exec(
-		'npm run prod:build && npm run prod:maintain && npm run prod:start',
-		(err, stdout, stderr) => {
-			if (err) {
-				console.error(err);
-			} else {
-				console.log(`stdout: ${stdout}`);
-				console.log(`stderr: ${stderr}`);
+const date = new Date();
+let currDate = date.getDate();
+
+// Will run every hour
+// Will only check for new Tweets and update GitHub repo if it is a new day
+const hourly = () => {
+	console.log('Checking for new Tweets and updating GitHub repo with new data...');
+	const newDate = new Date();
+	const tempDate = newDate.getDate();
+	if (tempDate !== currDate) {
+		currDate = tempDate;
+		exec(
+			'npm run prod:build && npm run prod:maintain && npm run prod:start',
+			(err, stdout, stderr) => {
+				if (err) console.error(err);
+				else {
+					console.log(stdout);
+					console.log(stderr);
+				}
 			}
-		}
-	);
-}, 1000 * 60 * 60 * 24);
+		);
+	}
+};
+setInterval(hourly, 1000 * 60 * 60);
