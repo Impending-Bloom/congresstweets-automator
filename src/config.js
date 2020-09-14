f67bb70af8a0e79cf4f4b389cbcf02c03fccf768
Will Magee <wmagee03@gmail.com>
2020-09-14 16:27:16 -0400

    added more env config variables

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 export const TWITTER_CONFIG = {
+  bearer_token: process.env.TWITTER_BEARER_AUTH_TOKEN,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  webhook_env: process.env.TWITTER_WEBHOOK_ENV,
-  list_id: process.env.TWITTER_LIST_ID,
+  webhook_prod: process.env.TWITTER_WEBHOOK_PROD_ENV,
+  webhook_dev: process.env.TWITTER_WEBHOOK_DEV_ENV,
+  app_id: process.env.TWITTER_APP_ID,
+  user_id: process.env.TWITTER_USER_ID,
+  list_id: process.env.TWITTER_CONGRESS_MEMBER_LIST_ID,
 };
 
 export const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-export const APP_CONFIG = {
-  TWITTER_CONFIG,
-  GOOGLE_APPLICATION_CREDENTIALS,
-}
+export const TIME_ZONE = process.env.TIME_ZONE || 'America/New_York';
 /* eslint-enable */
